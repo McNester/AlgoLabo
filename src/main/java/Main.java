f09940630088d87ce6f45bb2001469f68e000d2b
McNester <seraf5@bk.ru>
2024-04-04 17:20:20 +0500

    finished second problem, changed executeFromCmd method so that we can call first and second problem explicitly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,17 +13,36 @@ public class Main {
     private static void main() {
         Utils.printNewLine();
         runFirstProblem();
+        runSecondProblem();
 
     }
 
+    /*
+     * The problem runnners are here
+     */
     private static void runFirstProblem() {
         System.out.println("The first problem");
         Utils.printNewLine();
         int[] arr = Utils.getArrayInput();
         Utils.printNewLine();
-        System.out.println("The minimum val from the array:" + getMin(arr));
+        System.out.println("The minimum val from the array: " + getMin(arr));
 
     }
+
+    private static void runSecondProblem() {
+        Utils.printNewLine();
+        System.out.println("The second problem");
+        Utils.printNewLine();
+        int[] array = Utils.getArrayInput();
+        Utils.printNewLine();
+        System.out.println("The average from the array: " + getAvg(array));
+
+    }
+
+    /*
+     * Here are methods that are used to solve the problmes,
+     * but only pure solution, nothing related with inputs,user experience, etc.
+     */
 
     private static int getMin(int[] array) {
         int n = Integer.MAX_VALUE;
@@ -37,9 +56,42 @@ public class Main {
         return n;
     }
 
-    private static void executeFromCmd(String method) {
+    private static double getAvg(int[] array) {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        double avg = sum / (double) array.length;
 
-        System.out.println(method);
+        return avg;
+    }
+
+    /*
+     * Command line executioner
+     */
+
+    private static void executeFromCmd(String method) {
+        if (method.equals("firstProb")) {
+            runFirstProblem();
+        } else if (method.equals("secondProb")) {
+            runSecondProblem();
+        } else if (method.equals("thirdProb")) {
+            runSecondProblem();
+        } else if (method.equals("fourthProb")) {
+            runSecondProblem();
+        } else if (method.equals("fifthProb")) {
+            runSecondProblem();
+        } else if (method.equals("sixthProb")) {
+            runSecondProblem();
+        } else if (method.equals("seventhProb")) {
+            runSecondProblem();
+        } else if (method.equals("eighthProb")) {
+            runSecondProblem();
+        } else if (method.equals("ninthProb")) {
+            runSecondProblem();
+        } else if (method.equals("tenthProb")) {
+            runSecondProblem();
+        }
 
     }
 }
